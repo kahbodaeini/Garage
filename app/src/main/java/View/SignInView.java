0baab5acbf4479760d20273ea57e4111e7b916b1
kahbodaeini <kahbodaeini@gmail.com>
2021-07-08 13:36:55 +0430

    Sign up page view

---
+++ app/src/main/java/View/SignInView.java
@@ -0,0 +1,17 @@
+package View;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.garage.R;
+
+public class SignInView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstancesState) {
+        super.onCreate(savedInstancesState);
+        setContentView(R.layout.signup_layoout);
+
+    }
+}
