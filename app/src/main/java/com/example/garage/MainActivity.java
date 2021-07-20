f86c7e9522b35c8eb3a6af028f2dd452e854f876
mmdrez4 <mohammadreza.dn80@gmail.com>
2021-07-20 23:13:02 +0430

    update and complete add_car.xml

--- app/src/main/java/com/example/garage/MainActivity.java
+++ app/src/main/java/com/example/garage/MainActivity.java
@@ -39,7 +39,8 @@ private ActivityMainBinding binding;
 //            }
 //        });
     }
-@Override
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
