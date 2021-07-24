5b861274f9738b631b3ac168e8ef1c5cb66e0980
mmdrez4 <mohammadreza.dn80@gmail.com>
2021-07-24 15:05:16 +0430

    add CarsFragment.java and ProfileFragment.java and update UserActivity.java

--- app/src/main/java/com/example/garage/UserActivity.java
+++ app/src/main/java/com/example/garage/UserActivity.java
@@ -1,20 +1,67 @@
 package com.example.garage;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.ui.AppBarConfiguration;
-
-import com.example.garage.databinding.ActivityMainBinding;
+import com.example.garage.CarsFragment;
+import com.example.garage.ProfileFragment;
+import com.example.garage.R;
 
 public class UserActivity extends AppCompatActivity {
-
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
-
+    TabLayout tabLayout;
+    FrameLayout frameLayout;
+    Fragment fragment = null;
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.user_layout);
+        setContentView(R.layout.activity_main);
+
+        tabLayout=(TabLayout)findViewById(R.id.tabLayout);
+        frameLayout=(FrameLayout)findViewById(R.id.frameLayout);
+
+        fragment = new ProfileFragment();
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        fragmentTransaction.commit();
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                // Fragment fragment = null;
+                switch (tab.getPosition()) {
+                    case 0:
+                        fragment = new ProfileFragment();
+                        break;
+                    case 1:
+                        fragment = new CarsFragment();
+                        break;
+                }
+                FragmentManager fm = getSupportFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                ft.replace(R.id.frameLayout, fragment);
+                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+                ft.commit();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
     }
 }
