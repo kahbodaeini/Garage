5381de550118c7e4b6f591b546be2a41e0191464
mmdrez4 <mohammadreza.dn80@gmail.com>
2021-07-25 21:02:24 +0430

    update addPic in ProfileFragment.java

--- app/src/main/java/com/example/garage/ProfileFragment.java
+++ app/src/main/java/com/example/garage/ProfileFragment.java
@@ -1,18 +1,36 @@
 package com.example.garage;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 //import android.support.v4.app.Fragment;
+import android.os.Environment;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 
 import androidx.fragment.app.Fragment;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.channels.FileChannel;
 import java.util.zip.Inflater;
+
+import Controller.SignAndLog;
 
 public class ProfileFragment extends Fragment {
 
@@ -31,7 +49,48 @@ public class ProfileFragment extends Fragment {
                 buyHeadLightPopupWindow(view, inflater);
             }
         });
+        rootView.findViewById(R.id.change_pic).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    addPic(rootView.findViewById(R.id.imageView));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
         return rootView;
+    }
+
+    public void addPic(ImageView imageView) throws IOException {
+        loadImage(imageView);
+    }
+
+    public void loadImage(ImageView imageView) throws IOException {
+        String path = "/sdcard/Images/test_image.jpg";
+        File imgFile = new  File(path);
+        if(imgFile.exists()){
+            Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+            imageView.setImageBitmap(myBitmap);
+            saveImage(imgFile);
+        }
+    }
+
+    public void saveImage(File file) throws IOException {
+        String directory = "";
+        File newFile = new File(directory, file.getName());
+        FileChannel outputChannel = null;
+        FileChannel inputChannel = null;
+        try {
+            outputChannel = new FileOutputStream(newFile).getChannel();
+            inputChannel = new FileInputStream(file).getChannel();
+            inputChannel.transferTo(0, inputChannel.size(), outputChannel);
+            inputChannel.close();
+        } finally {
+            if (inputChannel != null) inputChannel.close();
+            if (outputChannel != null) outputChannel.close();
+        }
     }
 
     public void buyHeadLightPopupWindow(View view, LayoutInflater serviceInflater) {
