d0ac8963ca1114f1582bd8a95eeb42f83fd4b01e
mmdrez4 <mohammadreza.dn80@gmail.com>
2021-07-25 17:55:13 +0430

    debug ProfileFragment.java

--- app/src/main/java/com/example/garage/ProfileFragment.java
+++ app/src/main/java/com/example/garage/ProfileFragment.java
@@ -2,11 +2,17 @@ package com.example.garage;
 
 import android.os.Bundle;
 //import android.support.v4.app.Fragment;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 
 import androidx.fragment.app.Fragment;
+
+import java.util.zip.Inflater;
 
 public class ProfileFragment extends Fragment {
 
@@ -19,8 +25,38 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
-
+        rootView.findViewById(R.id.add_budget).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buyHeadLightPopupWindow(view, inflater);
+            }
+        });
         return rootView;
+    }
+
+    public void buyHeadLightPopupWindow(View view, LayoutInflater serviceInflater) {
+        // inflate the layout of the popup window
+        LayoutInflater inflater = (LayoutInflater) serviceInflater;
+        View popupView = inflater.inflate(R.layout.popup_window, null);
+
+        // create the popup window
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true; // lets taps outside the popup also dismiss it
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+        // show the popup window
+        // which view you pass in doesn't matter, it is only used for the window tolken
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+
+        // dismiss the popup window when touched
+        popupView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                popupWindow.dismiss();
+                return true;
+            }
+        });
     }
 
 }
--- app/src/main/java/com/example/garage/ServicesActivity.java
+++ app/src/main/java/com/example/garage/ServicesActivity.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
